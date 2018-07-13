c8db01bfbd72343c360e420ada0adc3e596f0291
honnuanand <anand@honnu.com>
2018-07-13 07:30:02 -0700

    back to 2.1

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace dotnet_webhello
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_0);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
